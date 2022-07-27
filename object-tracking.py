9a76da2e45763652f6ed212f738427d9065bd242
mpongkorn <104343838+mpongkorn@users.noreply.github.com>
2022-07-27 22:56:41 +0700

    Python object tracking on highway

---
+++ object-tracking.py
@@ -0,0 +1,39 @@
+import cv2
+
+cap = cv2.VideoCapture('highway.mp4')
+
+# object detection from stable camera
+#object_detector = cv2.createBackgroundSubtractorMOG2()
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+while True:
+	ret, frame = cap.read()
+	height, width, _ = frame.shape
+
+	# Extract region of interest
+	roi = frame[340:720, 500:800]
+
+	# object detection
+	#mask = object_detector.apply(frame)
+	mask = object_detector.apply(roi)
+	_, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	for cnt in contours:
+		# calculate area and remove small element
+		area = cv2.contourArea(cnt)
+		if area > 100:
+			#cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+			#cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
+			x, y, w, h = cv2.boundingRect(cnt)
+			cv2.rectangle(roi, (x,y), (x+w, y+h), (0,255,0), 2)
+
+	cv2.imshow('Frame', frame)
+	cv2.imshow('Mask', mask)
+	cv2.imshow('ROI', roi)
+ 
+	key =cv2.waitKey(30)
+	if key == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
